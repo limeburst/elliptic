407b59a295cd92f3e47dad644998f09f163d4e88
Jihyeok Seo <limeburst@member.fsf.org>
2015-09-29 23:23:33 +0900

    FFI for Curve25519 signature functions

--- curve25519-sys/src/lib.rs
+++ curve25519-sys/src/lib.rs
@@ -1,3 +1,21 @@
+extern crate libc;
+
+use libc::{c_int, c_ulong, c_uchar};
+
 #[test]
 fn it_works() {
+}
+
+#[link(name = "ref10_extract")]
+extern {
+    pub fn curve25519_keygen(curve25519_pubkey_out: *mut c_uchar,
+                             curve25519_privkey_in: *const c_uchar);
+    pub fn curve25519_sign(signature_out: *mut c_uchar,
+                           curve25519_privkey: *const c_uchar,
+                           msg: *const c_uchar,
+                           msg_len: c_ulong,
+                           random: *const c_uchar) -> c_int;
+    pub fn curve25519_verify(signature: *const c_uchar,
+                             curve25519_pubkey: *const c_uchar,
+                             msg: *const c_uchar) -> c_int;
 }
